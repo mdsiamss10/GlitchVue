31e0e8afbbdd0818e0aab5c8c13957fad168d552
mdsiamss10 <mdsiamss10@gmail.com>
2023-11-30 10:59:33 +0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="p-2">
+    <h1>Currently activated pages are {{ activePage }}</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activePage: 0
+    }
+  }
+}
+</script>
